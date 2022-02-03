5dd6dd664002af84dd168aafe51b7c40099bcfcc
esta-rossa <rajiabdou9@gmail.com>
2022-02-03 23:31:07 +0100

    feat: parse `README.md` file and replace old commit with recent one

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import { Octokit } from '@octokit/rest';
+import { readFile, writeFile } from 'fs/promises';
 
 type CommitInfoData = {
   message: string;
@@ -78,6 +79,58 @@ const assembleTheNewLine = (data: CommitInfoData): string => {
   return `${truncMessage} ${data.repo}@${data.sha}`;
 };
 
+/**
+ * open and parse the README file, then replace the commit line with latest one
+ * @param line: the new commit line
+ */
+const updateReadmeFile = async (line: string): Promise<boolean> => {
+  core.notice(`Reading README.md`);
+  const readmeFile = await readFile('./README.md', 'utf-8').catch((error) => {
+    if (error.code === 'ENOENT')
+      core.setFailed("This repository doesn't have README.md");
+    else core.setFailed(`Failed to read README.md, error: ${error.code}`);
+    return null;
+  });
+  if (!readmeFile) return false;
+
+  const readmeFileLines = readmeFile.split('\n');
+
+  let startI = readmeFileLines.findIndex(
+    (content) => content.trim() === '<!-- LATESTCOMMIT:START -->'
+  );
+
+  let endI = readmeFileLines.findIndex(
+    (content) => content.trim() === '<!-- LATESTCOMMIT:END -->'
+  );
+
+  if (startI === -1 || endI === -1) {
+    core.setFailed(
+      `Could not found \`<!-- LATESTCOMMIT:${
+        startI === -1 ? 'START' : 'END'
+      } -->\` in file`
+    );
+    return false;
+  }
+
+  readmeFileLines.splice(
+    startI + 1,
+    startI + 1 === endI ? 0 : endI - startI - 1,
+    line
+  );
+
+  const newFile = readmeFileLines.join('\n');
+
+  if (newFile === readmeFile) {
+    core.warning('No new commits nothing changed, not commits been done');
+    return false;
+  }
+
+  await writeFile('./README.md', newFile);
+  core.notice('Updated README');
+
+  return true;
+};
+
 async function run() {
   const username = core.getInput('GH_USERNAME');
 
@@ -90,5 +143,8 @@ async function run() {
   if (error || !data) return;
 
   const newLine = assembleTheNewLine(data);
+  core.notice(`Found commit in ${data.repo}`);
+
+  const updated = await updateReadmeFile(newLine);
 }
 run();
