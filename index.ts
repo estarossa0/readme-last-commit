1a291409c09fe08c62c1982237cf202a70cf900b
esta-rossa <rajiabdou9@gmail.com>
2022-02-03 20:10:47 +0100

    feat: add GitHub username input and log last 5 events

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log('hey');
+import * as core from '@actions/core';
+import { Octokit } from '@octokit/rest';
+
+async function run() {
+  const github = new Octokit({});
+  const username = core.getInput('GH_USERNAME');
+
+  console.log(username);
+  const data = await github.activity
+    .listPublicEventsForUser({ username: username, per_page: 5 })
+    .then(({ data }) => data);
+
+  console.log(data);
+}
+run();
